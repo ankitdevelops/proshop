c0bf00d583ecb24e5a33c3d6b5ed1b583e7256d3
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-05-30 12:06:48 +0530

    section -6 backend authentication

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const port = process.env.PORT || 8000;
 connectDB(); // Connect to mongodb
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
 app.use(notFound);
